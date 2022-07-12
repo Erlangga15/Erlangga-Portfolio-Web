af21bb17f4c3415ce247f1f0c24d47fd0037f1f5
Erlangga Hendrika <angga.hendrika@gmail.com>
2022-07-12 20:46:18 +0700

    Container Header

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { Header } from './container';
 import { Navbar } from './components';
 import './App.scss';
 
 const App = () => (
-	<div className="app">
-		<Navbar />
-	</div>
+  <div className="app">
+    <Navbar />
+    <Header />
+  </div>
 );
 
 export default App;
--- react_frontend/src/constants/images.js
+++ react_frontend/src/constants/images.js
@@ -1,5 +1,17 @@
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
+import code from '../assets/code.ico';
+
 export default {
+	node,
+	react,
+	profile,
+	circle,
 	logo,
+	code,
 };
