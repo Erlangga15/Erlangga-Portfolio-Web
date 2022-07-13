1c768b8b83e33683f69e4cdbf08cc2b5c036b827
Erlangga Hendrika <angga.hendrika@gmail.com>
2022-07-13 19:10:23 +0700

    Change About comp with data from Sanity

--- react_frontend/src/container/About/About.jsx
+++ react_frontend/src/container/About/About.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 import './About.scss';
+import { urlFor, client } from '../../client';
 
 const About = () => {
-	const abouts = [
-		{ title: 'Web Development', description: 'I am a web developer with 2 years of experience in web development', imgUrl: images.about01 },
-		{ title: 'Fullstack Developer', description: 'I am developing a website with the usual stack used is the MERN stack and am currently developing a web using serverless technology', imgUrl: images.about02},
-		{ title: 'System Analysis', description: 'I am developing a company management system by analyzing the company\'s system requirements to produce an ideal application system', imgUrl: images.about03},
-	];
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+
+		client.fetch(query).then((data) => {
+			setAbouts(data);
+		});
+	}, []);
 
 	return (
 		<>
 			<h2 className="head-text">
-				"A <span>successful</span> website does three things:<br />
-				It <span>attracts</span> the right kinds of visitors.<br />
-				<span>Guides</span> them to the main services or product you offer.<br />
+				"A <span>successful</span> website does three things:
+				<br />
+				It <span>attracts</span> the right kinds of visitors.
+				<br />
+				<span>Guides</span> them to the main services or product you offer.
+				<br />
 				<span>Collects</span> contact details for future ongoing relations"
 			</h2>
 
@@ -29,7 +36,7 @@ const About = () => {
 						className="app__profile-item"
 						key={about.title + index}
 					>
-						<img src={about.imgUrl} alt={about.title} />
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
 						<h2 className="bold-text" style={{ marginTop: 20 }}>
 							{about.title}
 						</h2>
