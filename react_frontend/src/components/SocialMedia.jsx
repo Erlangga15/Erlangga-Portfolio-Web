5b8de04a1ff4609581257f9d9688ed428231bdd4
Erlangga Hendrika <angga.hendrika@gmail.com>
2022-07-13 19:57:25 +0700

    Comp social media & navigation dot

---
+++ react_frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsInstagram, BsLinkedin } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsLinkedin />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
